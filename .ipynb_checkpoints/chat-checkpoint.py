c8155d3bbef2bda1a2f91e61130e4c5936dbce71
jaonkongie <jasonctkong@yahoo.com>
2024-04-23 07:53:09 +0000

    chat.py

--- .ipynb_checkpoints/chat-checkpoint.py
+++ .ipynb_checkpoints/chat-checkpoint.py
@@ -1,33 +1,57 @@
 from llama_cpp import Llama
 
+
+class Chat:
+    def __init__(self, model_path):
+        self.model = self.load_model(model_path)
+
+    def run(self):
+        while True:
+            print("enter quit to quit")
+            my_prompt = input("You: ")
+
+            if (my_prompt.lower() == "quit"):
+                break
+            
+            response = self.generate_text_from_prompt(my_prompt)
+            print(response)
+
+        print("finished running..")
+                
+
+    
+    # LOAD THE MODEL
+    def load_model(self, model_path):    
+        model = Llama(model_path=model_path,
+                            n_ctx=512)
+        return model
+    
+    # GENERATE TEXT FROM USER
+    def generate_text_from_prompt(self, 
+                                 user_prompt,
+                                 max_tokens = 100,
+                                 temperature = 0.3,
+                                 top_p = 0.1,
+                                 echo = True,
+                                 stop = ["Q", "\n"]):
+    
+       model_output = self.model(
+           user_prompt,
+           max_tokens=max_tokens,
+           temperature=temperature,
+           top_p=top_p,
+           echo=echo,
+           stop=stop,
+       )
+    
+    
+       return model_output
+
 def main():
     # Initialize the Llama model with the specified path to the .gguf model file
-    model_path = "./TinyLlama-1.1B-Chat-v1.0/TinyLlama.gguf"
-    llm = Llama(model_path=model_path)
-
-    print("Welcome to the TinyLlama Chatbot. Type 'quit' to exit.")
-
-    while True:
-        question = input("You: ")
-        print(question)
-        if question.lower() == 'quit':
-            break
-
-        response = llm.create_chat_completion(
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a helpful assistant that outputs in JSON.",
-                },
-                {"role": "user", "content": f"{question}"},
-            ],
-            response_format={
-                "type": "json_object",
-            },
-            temperature=0.7,
-        )
-
-        print(response) 
+    model_path = "../output/model.gguf"
+    chat = Chat(model_path)
+    chat.run()
 
 if __name__ == "__main__":
     main()
