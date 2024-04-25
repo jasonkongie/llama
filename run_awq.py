9b13677f3afb9b0b929076dc7fa46514ef7f96f2
jckongucsd <jckong@ucsd.edu>
2024-04-24 19:33:58 -0700

    calibrate method included

--- run_awq.py
+++ run_awq.py
@@ -2,13 +2,13 @@ import os
 import subprocess
 from awq import AutoAWQForCausalLM
 from transformers import AutoTokenizer
+from datasets import load_dataset
 
 
 model_path = 'TinyLlama-1.1B-Chat-v1.0'
 quant_path = './output/'
 llama_cpp_path = './new/'
 quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
-
 
 
 # Load model
@@ -18,14 +18,37 @@ model = AutoAWQForCausalLM.from_pretrained(
 )
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
+
+def calibrate():
+    # Load the dataset
+    dataset = load_dataset("lmsys/chatbot_arena_conversations")
+
+    # Define a function to concatenate question and response
+    def concatenate_data(examples):
+        concatenated_texts = []
+        for conversation in examples['conversation_a']:
+            if len(conversation) >= 2:  # Ensuring there are at least two turns in the conversation
+                concatenated_text = conversation[0]['content'] + " " + conversation[1]['content']
+                concatenated_texts.append(concatenated_text)
+        return {"text": concatenated_texts}
+
+    # Apply the concatenation to each example in the dataset
+    concatenated = dataset.map(concatenate_data, batched=True)
+
+    # Assuming we're working with the 'train' split of the dataset
+    return concatenated['train']['text']
+
+
 # Quantize
-# NOTE: We avoid packing weights, so you cannot use this model in AutoAWQ
-# after quantizing. The saved model is FP16 but has the AWQ scales applied.
-model.quantize(
-    tokenizer,
-    quant_config=quant_config,
-    export_compatible=True
-)
+model.quantize(tokenizer, quant_config=quant_config, calib_data=calibrate())
+
+
+#Discard the code below..
+# model.quantize(
+#     tokenizer,
+#     quant_config=quant_config,
+#     export_compatible=True
+# )
 
 # Save quantized model
 model.save_quantized(quant_path)
